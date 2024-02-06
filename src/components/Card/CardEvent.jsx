b3e9a0ced4677943e7de30fea3d4b01e65562d97
SHIVANSHU-TECH <Shivanshushuklacse@delhitechnicalcampus.ac.in>
2024-02-06 14:13:08 +0530

    donee

---
+++ src/components/Card/CardEvent.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from 'next/image';
+import "./card.css";
+const cardData = [
+  {
+    title: {},
+  },
+];
+const Card = (props) => {
+  return (
+    <div className="card">
+      <div className="cardImg">
+        <Image src={props.image}  className="img"/>
+        </div>
+      <div className="cardTitle">
+        <h4>{props.title}</h4>
+        <span>{props.date}</span>
+      </div>
+      <div className="cardDesc">
+        <p>{props.desc}</p>
+      </div>
+      <div className="btn-join">
+        <button><a href={props.mainLink}>Join Now</a></button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
